66994db5eac020fda7d8e06c6c6591b17771555e
Matheus Douglas Alves Vitor <matheusdouglas58@gmail.com>
2024-06-12 19:43:33 -0300

    env.ts

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const envSchema = z.object({
-  NODE_ENV: z.string(),
+  NODE_ENV: z.string().default("production"),
   DATABASE_URL: z.string(),
   DATABASE_CLIENT: z.enum(["pg"]),
   MONGOSE_URL: z.string(),
